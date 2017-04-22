c6a06793cb8597ccf12e2b81270c203d1ad226b0
kubotaku1119 <kubotaku1119@gmail.com>
2017-04-22 14:34:24 +0900

    initial commit

---
+++ app/src/main/java/spajam2017/haggy/yourrope/MainActivity.java
@@ -0,0 +1,13 @@
+package spajam2017.haggy.yourrope;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
